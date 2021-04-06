aa580643e55be7834f3085b11d69b11ea074068e
Dan Thompson <daniel.24.thompson@bt.com>
2021-04-06 20:52:04 +0100

    Completed the Login Page UI

---
+++ app/src/main/java/com/university/gradetrak/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.university.gradetrak
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.university.gradetrak.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun handleLoginButtonClick(view: View){
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
